54e864cbed692fc80842eddc2a345b3437849f27
JEAN-LOUIS Cher-Enfant Marvens <46737177+cher-enfant@users.noreply.github.com>
2024-07-07 04:00:59 -0400

    Update palendrome.py

---
+++ palendrome.py
@@ -0,0 +1,8 @@
+def palindrome(phrase):
+    phrase=phrase.lower.repl
+print("entre le mot ou la phease a tester:", end="")
+M=input()
+if (M==M[::-1]):
+    print(" c'est un palindrome")
+else:
+    print("c'est pas un palindrome")
