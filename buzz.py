798da8c9a0522dfc3620d081114ea4f7b31721ed
JEAN-LOUIS Cher-Enfant Marvens <46737177+cher-enfant@users.noreply.github.com>
2024-07-07 03:59:23 -0400

    Update buzz.py

---
+++ buzz.py
@@ -0,0 +1,22 @@
+print("entre votre valeur", end="")
+A=int(input())
+if(A%3==0 and A%5==0):
+    print("FIZZBUZZ")
+elif(A%5==0):
+        print("BUZZ")
+elif(A%3==0):
+    print("FIZZ")
+else:
+    print(A)   
+        
+print("entre votre valeur", end="")
+A=int(input())
+if(A%3==0 and A%5==0):
+    print("FIZZBUZZ")
+elif(A%5==0):
+        print("BUZZ")
+elif(A%3==0):
+    print("FIZZ")
+else:
+    print(A)   
+        
